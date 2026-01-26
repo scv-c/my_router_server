14278e89057dfd5534f910cc92a4dd2fe2a8ae8e
SCV <scv_c@naver.com>
2026-01-26 20:44:49 +0900

    fix: stripPath설정
    경로상 붙는 prefix 제거 설정 활성화.
    테스트 완료

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -7,7 +7,7 @@ export const routes: RouteConfig[] = [
     path: '/project-a', // 이 경로로 들어오면
     targetHost: process.env.PROJECT_A_HOST || "", // .env에서 관리
     targetPort: parseInt(process.env.PROJECT_A_PORT || '8080'),    
-    stripPath: false, // /blog 경로를 그대로 뒤단 서버로 넘길지 여부
+    stripPath: true, // /blog 경로를 그대로 뒤단 서버로 넘길지 여부
     security: {
       enableIpWhitelist: true, // 보안 옵션 예시
     },
@@ -16,7 +16,7 @@ export const routes: RouteConfig[] = [
     path: '/mreg',
     targetHost: process.env.LLM_REG_SERVICE_HOST || "",
     targetPort: parseInt(process.env.LLM_REG_SERVICE_PORT || ''),
-    stripPath: false, // /blog 경로를 그대로 뒤단 서버로 넘길지 여부
+    stripPath: true, // /blog 경로를 그대로 뒤단 서버로 넘길지 여부
   },
   // N개의 도메인 추가 가능...
 ];
