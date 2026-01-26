7c11d4dfa3521ff636e55c9dcef86c4a94c1fe5f
SCV <scv_c@naver.com>
2026-01-26 21:16:59 +0900

    fix: (임시)loggin추가
    왜 forbidden?

--- src/middleware/SecurityManager.ts
+++ src/middleware/SecurityManager.ts
@@ -1,6 +1,7 @@
 // src/middleware/SecurityManager.ts
 import { NextRequest } from 'next/server';
 import { RouteConfig } from '@/type/router';
+import { Logger } from './Logger';
 
 export class SecurityManager {
   // 허용된 관리자 IP (예시)
@@ -12,6 +13,11 @@ export class SecurityManager {
     // 1. IP Whitelist 체크
     if (config.security.enableIpWhitelist) {
       const ip = req.headers.get('x-forwarded-for') || '127.0.0.1';
+
+      //loggin추가
+      console.log(`자자자 ip값 찍어보자 : ${req.headers.get('x-forwarded-for')}`);
+      console.log(`자자자 IP WhiteList check 찍어보자 : ${JSON.stringify(this.WHITELISTED_IPS)}`);
+
       // 로컬 테스트 환경 고려하여 ::1 등 처리 필요할 수 있음
       if (!this.WHITELISTED_IPS.includes(ip.split(',')[0].trim())) {
         return false;
