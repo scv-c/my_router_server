3c99a562778610fce31c0e68befae89dfff75a37
SCV <scv_c@naver.com>
2026-01-28 00:32:57 +0900

    feat: prag추가 및 환경변수 변경
    personal rag 서비스 확장을 위한 router서비스 업데이트

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -2,21 +2,33 @@
 import { RouteConfig } from '@/type/router';
 
 // 2-4-1. 신규 서비스 오픈 시 이곳에 정의
-export const routes: RouteConfig[] = [
+export const routes: RouteConfig[] = [  
+  {
+    path: '/api/prag',
+    targetHost: process.env.PERRAG_API_SERVICE_HOST || "",
+    targetPort: parseInt(process.env.PERRAG_API_SERVICE_PORT || ''),
+    stripPath: true, // /blog 경로를 그대로 뒤단 서버로 넘길지 여부
+    security: {
+      enableIpWhitelist: false, // 보안 옵션 예시
+    }
+  },
   {
     path: '/project-a', // 이 경로로 들어오면
     targetHost: process.env.PROJECT_A_HOST || "", // .env에서 관리
     targetPort: parseInt(process.env.PROJECT_A_PORT || '8080'),    
     stripPath: true, // /blog 경로를 그대로 뒤단 서버로 넘길지 여부
     security: {
-      enableIpWhitelist: true, // 보안 옵션 예시
+      enableIpWhitelist: false, // 보안 옵션 예시
     },
   },
   {
-    path: '/mreg',
-    targetHost: process.env.LLM_REG_SERVICE_HOST || "",
-    targetPort: parseInt(process.env.LLM_REG_SERVICE_PORT || ''),
+    path: '/prag',
+    targetHost: process.env.PERRAG_WEB_SERVICE_HOST || "",
+    targetPort: parseInt(process.env.PERRAG_WEB_SERVICE_PORT || ''),
     stripPath: true, // /blog 경로를 그대로 뒤단 서버로 넘길지 여부
+    security: {
+      enableIpWhitelist: false, // 보안 옵션 예시
+    },
   },
   // N개의 도메인 추가 가능...
 ];
